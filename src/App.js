2a8452255ec01ba72afe2b4eca2b18d989f400e3
Kevin Jimenez <kevinja9608@gmail.com>
2020-02-22 14:38:07 -0600

    Private rout

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@
 
 import React from "react";
 import NavBar from "./components/NavBar";
-import { useAuth0 } from "./react-auth0-spa";
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  const { loading } = useAuth0();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
     </div>
   );
 }
