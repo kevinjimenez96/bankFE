1fb32d920b23da56d0b228fed7db82a1fdc94ae5
Kevin Jimenez <kevinja9608@gmail.com>
2020-02-28 05:56:18 -0600

    Landingpage

--- src/components/landing-page/landing-page.js
+++ src/components/landing-page/landing-page.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { useAuth0 } from "../../react-auth0-spa";
+import { Button } from "antd";
 
 function LandingPage() {
   const { loginWithRedirect } = useAuth0();
 
   return (
     <div className="landing-page">
-      <button onClick={() => loginWithRedirect({})}>Log in</button>
-      LandingPage
+      <img src="/full.png" alt="" className="landing-page__img" />
+      <Button type="primary" onClick={() => loginWithRedirect({})}>
+        Log in
+      </Button>
     </div>
   );
 }
--- src/components/transaction-item/transaction-item.js
+++ src/components/transaction-item/transaction-item.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../card/card";
 import CardHeader from "../card-header/card-header";
+import useAxios from "@use-hooks/axios";
 
 const TransactionItem = ({ transaction }) => {
+  const [accountTo, setaccountTo] = useState();
+  const accountAxios = useAxios({
+    url: `http://localhost:8080/v1/account/${transaction.accountTo.id}`,
+    method: "GET",
+    options: {
+      headers: {
+        Authorization: `Bearer ${window.sessionStorage.getItem("token")}`
+      }
+    }
+  });
+
+  useEffect(() => {
+    accountAxios.reFetch();
+  }, []);
+
+  useEffect(() => {
+    if (accountAxios.response != null && accountAxios.response.data !== "") {
+      console.log(accountAxios.response.data);
+      setaccountTo(accountAxios.response.data);
+    }
+  }, [accountAxios.response]);
+
+  let date = new Date(transaction.dateTime);
+  date = `${("00" + date.getDate()).slice(-2)}-${(
+    "00" +
+    (date.getMonth() + 1)
+  ).slice(-2)}-${date.getFullYear()}`;
   return (
     <Card className="card--small">
       <CardHeader
         currency={transaction.currency}
         amount={transaction.amount}
         title={transaction.detail}
-        secondary={"sedafffs"}
+        secondary={`${("0000000000000000" + transaction.accountFrom.id).slice(
+          -16
+        )} ----> ${("0000000000000000" + transaction.accountTo.id).slice(-16)}`}
         redAmount={transaction.red}
       />
+      <div className="transaction-item__date">{date}</div>
     </Card>
   );
 };
